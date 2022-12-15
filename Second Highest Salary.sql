06997fb80cdb636ed275d7b6d2dd92e09d3777cf
anurag2444 <64956535+anurag2444@users.noreply.github.com>
2022-12-15 16:15:23 +0530

    Create Second Highest Salary.sql

---
+++ Second Highest Salary.sql
@@ -0,0 +1,77 @@
+SQL Schema
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key column for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write an SQL query to report the second highest salary from the Employee table. If there is no second highest salary, the query should report null.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+
+
+
+
+
+Solution----1
+=====================================================================
+SELECT
+IFNULL(
+(SELECT salary 
+FROM
+(SELECT salary , ROW_NUMBER() OVER(ORDER BY salary DESC) AS temp
+FROM Employee) x
+WHERE temp=2),
+NULL)
+AS SecondHighestSalary
+
+
+
+Solution -2
+===========================================================
+SELECT MAX(Salary) AS SecondHighestSalary
+FROM Employee
+WHERE Salary < (SELECT MAX(Salary) FROM Employee)
+
+
